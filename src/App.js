8f10c342cf607e685a4636ddd47d171b677520fc
Jason Tjahjono <jasontjahjono.webdev@gmail.com>
2020-07-22 10:43:36 +0700

    Create main layout

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {Route, Switch} from "react-router-dom";
+import PaletteList from "./PaletteList";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import {generatePalette} from "./colorHelpers";
+
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>      
+        <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps}/>}/>
+        <Route 
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette palette={generatePalette(
+              this.findPalette(routeProps.match.params.id)
+            )}/>
+          )}
+        />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1>Single Color Page!</h1>}
+        />
+      </Switch>
+    );
+  }
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import Slider from 'rc-slider';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import 'rc-slider/assets/index.css';
+import './Navbar.css';
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {format: "hex", open: false};
+        this.handleFormatChange = this.handleFormatChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
+    }
+    handleFormatChange(e) {
+        this.setState({format: e.target.value, open: true});
+        this.props.handleChange(e.target.value);
+    }
+    closeSnackbar() {
+        this.setState({open: false});
+    }
+    render() {
+        const {level, changeLevel} = this.props;
+        const {format} = this.state;
+        return (
+            <header className="Navbar">
+                <div className="logo">
+                    <Link to="/">reactcolorpicker</Link>
+                </div>
+                <div className="slider-container">
+                    <span>Level: {level}</span>
+                    <div className="slider">
+                        <Slider 
+                            defaultValue={level}
+                            min={100}
+                            max={900}
+                            step={100}
+                            onAfterChange={changeLevel}
+                            trackStyle={{backgroundColor: 'transparent'}}
+                            handleStyle={{
+                            borderColor: 'green',
+                            height: 13,
+                            width: 13,
+                            marginLeft: -7,
+                            marginTop: -3,
+                            backgroundColor: 'green',
+                            boxShadow: 'none',
+                            }}
+                            railStyle={{height: 8}}
+                        />
+                    </div>
+                </div>
+                <div className="select-container">
+                    <Select onChange={this.handleFormatChange} value={format}>
+                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
+                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value="rgba">HEX - rgba(255,255,255,1.0)</MenuItem>
+                    </Select>
+                </div>
+                <Snackbar
+                    anchorOrigin={{vertical: "bottom", horizontal: "left"}}
+                    open={this.state.open}
+                    autoHideDuration={3000}
+                    message={<span id="message-id">Format Changed to {format.toUpperCase()}</span>}
+                    ContentProps={{
+                        "aria-describedby": "message-id"
+                    }}
+                    onClose={this.closeSnackbar}
+                    action={[
+                        <IconButton 
+                            onClick={this.closeSnackbar}
+                            color="inherit"
+                            key="close"
+                            aria-label="close"
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    ]}
+                />
+            </header>
+        )
+    }
+}
+
+export default Navbar;
