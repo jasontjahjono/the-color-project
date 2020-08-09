8dbac6f1cd674b9f354c788d8d72552cfffba356
Jason Tjahjono <jasontjahjono.webdev@gmail.com>
2020-08-09 16:14:08 +0700

    Fix Navbar Slider Size and Add clsx to ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import styles from './styles/ColorBoxStyles';
 import {withStyles} from "@material-ui/styles";
 import {CopyToClipboard} from "react-copy-to-clipboard";
+import clsx from 'classnames';
 
 class ColorBox extends Component {
     constructor(props) {
@@ -23,8 +24,8 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{background}} className={classes.ColorBox}>
-                    <div style={{background}} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} />
-                    <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
+                    <div style={{background}} className={clsx(classes.copyOverlay, {[classes.showOverlay]: copied})}/>
+                    <div className={clsx(classes.copyMessage, {[classes.showMessage]: copied})}>
                         <h1>copied!</h1>
                         <p className={classes.copyText}>{background}</p>
                     </div>
